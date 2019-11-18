1ff46494deb2f5192a569420612b3027c52a4126
Vincent Quigley <vquigley@moderndemocracy.com>
2019-11-18 17:50:55 +0000

    test

--- generic_build/entrypoint.sh
+++ generic_build/entrypoint.sh
@@ -5,7 +5,9 @@ echo "Working Directory: $1"
 echo "Package Name: $2"
 echo "Build Number: $3"
 cd $1
-echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" > ~/.npmrc
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" > .npmrc
+ls
+cat .npmrc
 npm install
 sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket $2
 sam publish --template packaged.yaml --semantic-version $3
