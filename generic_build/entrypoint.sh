3ba98551b6b279a60d0377d2f2d4ffd7d17fe17a
Vincent Quigley <vquigley@moderndemocracy.com>
2019-11-22 15:26:29 +0000

    test

--- generic_build/entrypoint.sh
+++ generic_build/entrypoint.sh
@@ -4,10 +4,10 @@ ls
 echo "Working Directory: $1"
 echo "Package Name: $2"
 echo "Build Number: $3"
+
+echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"
+echo "AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION"
 cd $1
-echo "registry=https://npm.pkg.github.com/moderndemocracyltd" > .npmrc
-ls
-cat .npmrc
-npm install
 sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket $2
 sam publish --template packaged.yaml --semantic-version $3
